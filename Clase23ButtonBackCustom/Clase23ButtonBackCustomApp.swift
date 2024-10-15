1a91c49da7cf70921f91f7c4e2d646fab4b2eebe
escurradeveloper <mescurrac123@gmail.com>
2024-10-15 14:48:20 -0500

    boton de retorno customizado en swiftui

---
+++ Clase23ButtonBackCustom/Clase23ButtonBackCustomApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clase23ButtonBackCustomApp.swift
+//  Clase23ButtonBackCustom
+//
+//  Created by Escurra Colquis on 15/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Clase23ButtonBackCustomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
